0fc60e176fac6c9c16feb901518a28928920c892
ka <konstagpov123@gmail.com>
2023-02-26 13:03:11 -0800

    following  https://github.com/vercel/next.js/tree/canary/examples/github-pages

--- next.config.js
+++ next.config.js
@@ -2,19 +2,13 @@
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-let assetPrefix = '';
 let basePath = '';
 
 if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
+  basePath = `/my-web-site`;
 }
 
 const nextConfig = {
-  assetPrefix: assetPrefix,
   basePath: basePath,
   experimental: {
     appDir: true,
