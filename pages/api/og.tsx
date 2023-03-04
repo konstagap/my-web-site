5a454ec96aa5126bc264c18a38d53a56bd28b8ed
ka <konstagpov123@gmail.com>
2023-03-04 08:46:57 -0800

    try with background img

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,6 +1,5 @@
 import { ImageResponse } from '@vercel/og';
 import { NextRequest } from 'next/server';
-import Image from 'next/image';
 
 export const config = {
   runtime: 'edge',
@@ -12,29 +11,25 @@ export default async function handler(req: NextRequest) {
 
   return new ImageResponse(
     (
-      <div style={{ height: '100%', width: '100%' }} tw='dark'>
-        <div tw='flex flex-col md:flex-row justify-start items-center gap-5 p-10'>
-          <Image
-            alt='Kostiantyn Agapov'
-            tw='rounded-full'
-            src='/Kostiantyn_Agapov.png'
-            width={150}
-            height={150}
-            priority
-          />
-          <div>
-            <h1 tw='text-2xl font-bold'>Kostiantyn Agapov</h1>
-            <p tw='py-4'>Software Engineer</p>
-            <p tw='py-3'>
-              <span>🇺🇦 </span> {postTitle || ''}
-            </p>
-          </div>
+      <div
+        style={{
+          height: '100%',
+          width: '100%',
+          backgroundRepeat: 'no-repeat',
+          backgroundImage: 'url(https://www.findkostas.com/og-bg.png)',
+        }}
+        tw='dark'
+      >
+        <div tw='ml-[300px] pt-10'>
+          <h1 tw='text-4xl font-bold'>Kostiantyn Agapov</h1>
+          <p tw='py-4'>Software Engineer</p>
+          <p tw='py-3'>🇺🇦 {postTitle || ''}</p>
         </div>
       </div>
     ),
     {
-      width: 1920,
-      height: 1080,
+      width: 800,
+      height: 300,
       debug: true,
     }
   );
