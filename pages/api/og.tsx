d33324aa3d0276833240e41f0e44e02b25d486e8
konstantin <konstantyn_agapov@intuit.com>
2023-03-03 22:28:04 -0800

    Seo Card fix

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -11,8 +11,8 @@ export default async function handler(req: NextRequest) {
 
   return new ImageResponse(
     (
-      <div tw='w-full h-screen dark flex justify-center items-center'>
-        <div tw=' flex flex-col md:flex-row justify-center items-center gap-5 shadow-2xl p-10 rounded-lg '>
+      <div style={{ height: '100%', width: '100%' }}>
+        <div tw='flex flex-col md:flex-row justify-start items-center gap-5 shadow-2xl p-10 rounded-lg'>
           <div tw='avatar'>
             <div tw='w-48 mask mask-squircle '>
               <img src='/Kostiantyn_Agapov.png' />
@@ -21,7 +21,6 @@ export default async function handler(req: NextRequest) {
           <div>
             <h1 tw='text-5xl font-bold'>Kostiantyn Agapov</h1>
             <p tw='py-4'>Software Engineer</p>
-            <p tw='py-3'>🇺🇦</p>
           </div>
         </div>
       </div>
