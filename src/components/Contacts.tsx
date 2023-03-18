230c6d3b9150c5ab92cc5e79dc0b4bf6222948e3
ka <konstagpov123@gmail.com>
2023-03-18 16:15:00 -0700

    adding new components to mdx

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -4,63 +4,52 @@ import React from 'react';
 import { DownloadIcon, EmailIcon, GitHubIcon, LinkedInIcon, PhoneIcon } from './icons';
 
 const Contacts = () => {
+  const list = [
+    {
+      href: 'tel:+19292945554',
+      icon: <PhoneIcon />,
+    },
+    {
+      href: 'mailto:konstagapov123@gmail.com',
+      icon: <EmailIcon />,
+    },
+    {
+      href: 'https://www.linkedin.com/in/kostiantyn-agapov',
+      rel: 'noopener noreferrer',
+      target: '_blank',
+      icon: <LinkedInIcon />,
+    },
+    {
+      href: 'https://github.com/konstagap',
+      rel: 'noopener noreferrer',
+      target: '_blank',
+      icon: <GitHubIcon />,
+    },
+    {
+      href: '/cv/Kostiantyn_Agapov_cv.pdf',
+      icon: <DownloadIcon />,
+      download: true,
+    },
+  ];
+
   return (
     <header className='prose prose-base text-center mb-5 px-1'>
-      <div className='flex w-full gap-4 justify-around items-center md:hidden'>
-        <a className='btn flex-1' href='tel:+19292945554'>
-          <PhoneIcon />
-        </a>
-        <a className='btn flex-1' href='mailto:konstagapov123@gmail.com'>
-          <EmailIcon />
-        </a>
-        <a
-          className='btn flex-1'
-          rel='noopener noreferrer'
-          target='_blank'
-          href='https://www.linkedin.com/in/kostiantyn-agapov'
-        >
-          <LinkedInIcon />
-        </a>
-        <a className='btn flex-1' rel='noopener noreferrer' target='_blank' href='https://github.com/konstagap'>
-          <GitHubIcon />
-        </a>
-        <Link className='btn flex-1' href='/cv/Kostiantyn_Agapov_cv.pdf' download>
-          <DownloadIcon />
-        </Link>
-      </div>
-
-      <div className='hidden md:block'>
-        <div className='flex gap-6 w-full justify-center mb-6'>
-          <a href='tel:+19292945554' title='(929)-294-5554'>
-            <PhoneIcon />
-            {/* <span>+1 (929) 294-5554</span> */}
-          </a>
-          <a href='mailto:konstagapov123@gmail.com' title='konstagapov123@gmail.com'>
-            <EmailIcon />
-          </a>
-          <a
-            rel='noopener noreferrer'
-            target='_blank'
-            href='https://github.com/konstagap'
-            title='https://github.com/konstagap'
-          >
-            <GitHubIcon />
-          </a>
-
-          <a
-            rel='noopener noreferrer'
-            target='_blank'
-            href='https://www.linkedin.com/in/kostiantyn-agapov'
-            title='https://www.linkedin.com/in/kostiantyn-agapov'
-          >
-            <LinkedInIcon />
-          </a>
-
-          <Link href='/cv/Kostiantyn_Agapov_cv.pdf' title='Kostiantyn_Agapov_cv.pdf' download>
-            <DownloadIcon />
-          </Link>
-        </div>
-        {/* <DowloadResume /> */}
+      <div className='btn-group  btn-group-horizontal'>
+        {list.map(({ icon, ...rest }, inx) => {
+          if (!rest.download) {
+            return (
+              <a key={inx} className='btn btn-md md:btn-sm hover:btn-accent' {...rest}>
+                {icon}
+              </a>
+            );
+          } else {
+            return (
+              <Link key={inx} className='btn btn-md md:btn-sm hover:btn-accent' {...rest}>
+                {icon}
+              </Link>
+            );
+          }
+        })}
       </div>
     </header>
   );
